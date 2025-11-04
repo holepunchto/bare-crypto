77ca612ba4156d926cc3bb9cc9496251a98d5f25
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-11-04 10:32:32 +0100

    Update lib/signature.d.ts

--- lib/signature.d.ts
+++ lib/signature.d.ts
@@ -1,7 +1,7 @@
 import Buffer from 'bare-buffer'
 import { Key as CryptoKey } from './key'
 
-export type SignatureAlgorithm = 'ED25519'
+export type SignatureAlgorithm = 'ed25519'
 
 export function sign(algorithm: null, data: ArrayBuffer | ArrayBufferView, key: CryptoKey): Buffer
 
