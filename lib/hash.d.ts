b5810fbd5d7c509ce31eb280c85b2e3f54ff0681
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-06-05 16:25:39 +0200

    Fix SHA-384 `generateKey()` which would previously throw (#41)

--- lib/hash.d.ts
+++ lib/hash.d.ts
@@ -1,7 +1,14 @@
 import { Transform, TransformOptions } from 'bare-stream'
 import Buffer, { BufferEncoding } from 'bare-buffer'
 
-type HashAlgorithm = 'md5' | 'sha-1' | 'sha-256' | 'sha-512' | 'blake2b-256' | 'ripemd-160'
+type HashAlgorithm =
+  | 'md5'
+  | 'sha-1'
+  | 'sha-256'
+  | 'sha-384'
+  | 'sha-512'
+  | 'blake2b-256'
+  | 'ripemd-160'
 
 declare class CryptoHash extends Transform {
   constructor(algorithm: HashAlgorithm | number, opts?: TransformOptions<CryptoHash>)
